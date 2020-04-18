b6f0202d94350bba36b7baed874211b1fdafe9a3
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-04-18 13:11:30 +0200

    Character maniability

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,9 +1,18 @@
 extends KinematicBody2D
 
-var speed = 0
+var speed = 3
 
 func _process(delta):
-	if speed < 50 :
-		speed += Input.get_action_strength("speed_up")
-	speed *= 0.95
-	move_and_slide(Vector2(speed*100, -speed*100))
+	var displacment = Vector2(3, -1.5)
+	if Input.is_action_pressed("ui_left"):
+		displacment.y -= 1.5
+	elif Input.is_action_pressed("ui_right"):
+		displacment.y += 1.5
+	
+	displacment = displacment.normalized()*300
+	
+	if Input.is_action_pressed("speed_down"):
+		displacment.x -= Input.get_action_strength("speed_down")*100
+		displacment.y += Input.get_action_strength("speed_down")*50
+	
+	move_and_slide(displacment)
