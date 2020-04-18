e7db5451759e7507458f4f63d7c739799145cd34
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-04-18 19:15:30 +0200

    Create gui

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 var speed = 3
 var direction = 1
 var return_possible = true
+var childs = 0
+
+signal pennywise
 
 func _process(delta):
 	var displacment = Vector2(3, -1.5)
@@ -27,7 +30,18 @@ func _process(delta):
 
 func _on_TrapsDetector_area_entered(area):
 	print("hit!")
+	childs = 0
 
 
 func _on_ChildDetector_area_entered(area):
 	print("Give me the child")
+	childs += 1
+
+
+func _on_PennywiseDetector_area_entered(area):
+	print("You'll be the last one")
+	direction = 1
+	emit_signal("pennywise", childs)
+
+func _on_PennywiseDetector_area_exited(area):
+	return_possible = true
