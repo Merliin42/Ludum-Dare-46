7af668c6f2a08c52073bd1c3f4545062277586b5
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-04-19 14:23:24 +0200

    Child counter

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,16 +1,26 @@
 extends Node
 
 var hungry = 60
+var childs = 0
 
 func _ready():
 	randomize()
 
-func _on_Character_pennywise(childs):
+func _on_Character_pennywise():
 	hungry += 10*childs
+	childs = 0
+	$GUI/ChildCounter/Label.text = str(childs)
 
 func _on_PennywiseHunger_timeout():
 	hungry -= 1
-	$GUI/MarginContainer/TextureProgress.value = hungry
+	$GUI/HungryBar/TextureProgress.value = hungry
 	$PennywiseHunger.start()
 
+func _on_Character_child():
+	childs += 1
+	$GUI/ChildCounter/Label.text = str(childs)
 
+
+func _on_Character_hit():
+	childs = 0
+	$GUI/ChildCounter/Label.text = str(childs)
