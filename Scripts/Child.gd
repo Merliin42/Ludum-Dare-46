cc8ddacf4e39062afe1fc88c1cc70f2e0be36ea3
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-04-19 08:53:07 +0200

    Gerbe de sang

--- Scripts/Child.gd
+++ Scripts/Child.gd
@@ -6,5 +6,18 @@ func _process(delta):
 	position.x += direction.x*delta
 	position.y += direction.y*delta
 
-func _on_Area2D_body_entered(body):
+func dead():
+	self.texture = null
+	$Blood.emitting = true
+	$Timer.start()
+
+func _on_Timer_timeout():
+	queue_free()
+
+
+func _on_Death_area_entered(area):
+	dead()
+
+
+func _on_Collisions_body_entered(body):
 	queue_free()
