42ac7477c33ccf5b63da5906a9d4ed0f8386b208
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-04-20 08:32:10 +0200

    PNJ fade out

--- Scripts/Child.gd
+++ Scripts/Child.gd
@@ -11,6 +11,13 @@ func dead():
 	$Blood.emitting = true
 	$Timer.start()
 
+func fade_out():
+	var start_color = Color(1.0, 1.0, 1.0, 1.0)
+	var end_color = Color(1.0, 1.0, 1.0, 0.0)
+	$Tween.interpolate_property(self, "modulate", start_color, end_color, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.start()
+
+
 func _on_Timer_timeout():
 	queue_free()
 
@@ -20,8 +27,12 @@ func _on_Death_area_entered(area):
 
 # warning-ignore:unused_argument
 func _on_Collisions_body_entered(body):
-	queue_free()
+	fade_out()
 
 
 func _on_Death_body_entered(body):
 	dead()
+
+
+func _on_Tween_tween_completed(object, key):
+	queue_free()
